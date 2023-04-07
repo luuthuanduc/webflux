e74e097f69ed9d538b9612a19dda6aa45496f46c
luuthuanduc <ducluuthuan@gmail.com>
2023-04-07 17:08:06 +0700

    CORS

--- src/main/java/com/aimatrix/webflux/web/CustomerRestController.java
+++ src/main/java/com/aimatrix/webflux/web/CustomerRestController.java
@@ -4,7 +4,6 @@ import com.aimatrix.webflux.models.CustomerModel;
 import com.aimatrix.webflux.services.CustomerService;
 import lombok.AllArgsConstructor;
 import org.reactivestreams.Publisher;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,15 +11,11 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
 
 @AllArgsConstructor
 @RestController
-@RequestMapping(
-    produces = MediaType.APPLICATION_JSON_VALUE,
-    consumes = MediaType.APPLICATION_JSON_VALUE)
 public class CustomerRestController {
 
     CustomerService customerService;
--- src/main/java/com/aimatrix/webflux/web/CustomerRouter.java
+++ src/main/java/com/aimatrix/webflux/web/CustomerRouter.java
@@ -3,11 +3,21 @@ package com.aimatrix.webflux.web;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.RouterFunctions;
 import org.springframework.web.reactive.function.server.ServerResponse;
 
-import static org.springframework.web.reactive.function.server.RequestPredicates.*;
+import java.util.List;
+
+import static org.springframework.web.reactive.function.server.RequestPredicates.DELETE;
+import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
+import static org.springframework.web.reactive.function.server.RequestPredicates.POST;
+import static org.springframework.web.reactive.function.server.RequestPredicates.PUT;
+import static org.springframework.web.reactive.function.server.RequestPredicates.accept;
+import static org.springframework.web.reactive.function.server.RequestPredicates.path;
 
 @Configuration
 class CustomerRouter {
@@ -21,6 +31,20 @@ class CustomerRouter {
             .andRoute(DELETE("/customers/{id}").and(accept(MediaType.APPLICATION_JSON)), handler::deleteCustomer)
             .andRoute(PUT("/customers/{id}").and(accept(MediaType.APPLICATION_JSON)), handler::putCustomer)
         );
+    }
+
+    @Bean
+    CorsWebFilter corsWebFilter() {
+        CorsConfiguration configuration = new CorsConfiguration();
+
+        configuration.setAllowedOrigins(List.of("http://localhost:8080"));
+        configuration.setAllowedMethods(List.of("POST", "GET", "PUT", "DELETE"));
+        configuration.setAllowedHeaders(List.of("X-USER-ID"));
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+
+        return new CorsWebFilter(source);
     }
 
 }
